e45a77147f159e39d273d5faa81a5d9bd27ae593
Roberto Rocha <rrocha.roberto@gmail.com>
2021-03-16 20:28:42 -0300

    Criando um projeto para iniciar a lista.

---
+++ Aula02/src/br/edu/univas/si3/ed/list/Product.java
@@ -0,0 +1,11 @@
+package br.edu.univas.si3.ed.list;
+
+public class Product {//nó (node)
+
+    //informações do nó
+    public String name;
+    public int amount;
+
+    //variável de controle da própria lista
+    public Product next;
+}
