2d68cd5539ec3f04ebcff29aef298373d4ba198e
Roberto Rocha <rrocha.roberto@gmail.com>
2021-04-15 22:32:15 -0300

    Implementando o main da Pilha e da Fila.

--- Projeto03/src/br/edu/univas/si3/ed/RunnerFila.java
+++ Projeto03/src/br/edu/univas/si3/ed/RunnerFila.java
@@ -1,8 +1,42 @@
 package br.edu.univas.si3.ed;
 
+import br.edu.univas.si3.ed.data.Queue;
+import br.edu.univas.si3.ed.model.Product;
+
 public class RunnerFila {
     public static void main(String[] args) {
         System.out.println("Testando a fila");
+
+        Queue fila = new Queue();
+        
+        System.out.println(fila.remove());//a fila está vazia
+        
+        Product p1 = new Product();
+        p1.name = "p1";
+        p1.amount = 9;
+        fila.insert(p1); //p1 entra na fila
+        
+        System.out.println(fila.remove());//p1 sai na fila
+
+        Product p2 = new Product();
+        p2.name = "p2";
+        p2.amount = 5;
+        fila.insert(p2); //p2 entra na fila
+
+        Product p3 = new Product();
+        p3.name = "p3";
+        p3.amount = 9;
+        fila.insert(p3); //p3 entra na fila
+
+        System.out.println(fila.remove());//p2 sai na fila
+
+        Product p4 = new Product();
+        p4.name = "p4";
+        p4.amount = 7;
+        fila.insert(p4); //p4 entra na fila
+
+        System.out.println(fila.remove());//p3 sai na fila
+        System.out.println(fila.remove());//p4 sai na fila
+        System.out.println(fila.remove());//a fila está vazia
     }
-    
 }
--- Projeto03/src/br/edu/univas/si3/ed/RunnerPilha.java
+++ Projeto03/src/br/edu/univas/si3/ed/RunnerPilha.java
@@ -1,7 +1,40 @@
 package br.edu.univas.si3.ed;
 
+import br.edu.univas.si3.ed.data.Stack;
+import br.edu.univas.si3.ed.model.Product;
+
 public class RunnerPilha {
     public static void main(String[] args) {
         System.out.println("Testando a pilha");
+
+        Stack pilha = new Stack();
+        System.out.println(pilha.pop());//a pilha está vazia
+
+        Product p1 = new Product();
+        p1.name = "p1";
+        p1.amount = 9;
+        pilha.push(p1); //empilha o p1
+
+        Product p2 = new Product();
+        p2.name = "p2";
+        p2.amount = 5;
+        pilha.push(p2); //empilha o p2
+
+        System.out.println(pilha.pop());//desempilha o p2
+
+        Product p3 = new Product();
+        p3.name = "p3";
+        p3.amount = 9;
+        pilha.push(p3); //empilha o p3
+
+        Product p4 = new Product();
+        p4.name = "p4";
+        p4.amount = 7;
+        pilha.push(p4); //empilha o p4
+
+        System.out.println(pilha.pop());//desempilha o p4
+        System.out.println(pilha.pop());//desempilha o p3
+        System.out.println(pilha.pop());//desempilha o p1
+        System.out.println(pilha.pop());//a pilha está vazia
     }
 }
