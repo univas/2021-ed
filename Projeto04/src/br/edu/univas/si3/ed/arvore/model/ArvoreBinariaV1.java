de30043813467fd919e56259588d2933842c3c3a
Roberto Rocha <rrocha.roberto@gmail.com>
2021-05-12 10:19:25 -0300

    Melhoria visual.

--- Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
+++ Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
@@ -2,24 +2,24 @@ package br.edu.univas.si3.ed.arvore.model;
 
 public class ArvoreBinariaV1 {
 
-    public No criarNo(Character caracter) {
-        No no = new No();
-        no.info = caracter;
-        return no;
-    }
+	public No criarNo(Character caracter) {
+		No no = new No();
+		no.info = caracter;
+		return no;
+	}
 
-    public No criarSubArvore(Character caracter, No esquerda, No direita) {
-        No pai = criarNo(caracter);
-        pai.dir = direita;
-        pai.esq = esquerda;
-        return pai;
-    }
+	public No criarSubArvore(Character caracterRaiz, No esquerda, No direita) {
+		No pai = criarNo(caracterRaiz);
+		pai.esq = esquerda;
+		pai.dir = direita;
+		return pai;
+	}
 
-    public void imprimirPreOrdem(No no) {
-        if (no != null) {
-            System.out.print(no.info + "-");
-            imprimirPreOrdem(no.esq);
-            imprimirPreOrdem(no.dir);
-        }
-    }
+	public void imprimirPreOrdem(No no) {
+		if (no != null) {
+			System.out.print(no.info + "-");
+			imprimirPreOrdem(no.esq);
+			imprimirPreOrdem(no.dir);
+		}
+	}
 }
