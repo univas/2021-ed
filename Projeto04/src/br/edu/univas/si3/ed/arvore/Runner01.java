4263783ee95262d9c2f92aa2f8e5afde4daf6bce
Roberto Rocha <rrocha.roberto@gmail.com>
2021-05-25 15:03:30 -0300

    Impressão Em-ordem e Pós-ordem

--- Projeto04/src/br/edu/univas/si3/ed/arvore/Runner01.java
+++ Projeto04/src/br/edu/univas/si3/ed/arvore/Runner01.java
@@ -4,17 +4,26 @@ import br.edu.univas.si3.ed.arvore.model.ArvoreBinariaV1;
 import br.edu.univas.si3.ed.arvore.model.No;
 
 public class Runner01 {
-    public static void main(String[] args) {
-        ArvoreBinariaV1 arvore = new ArvoreBinariaV1();
-    
-        No noD = arvore.criarNo('d');
-        No noE = arvore.criarNo('e');
-        No noF = arvore.criarNo('f');
-        
-        No noB = arvore.criarSubArvore('b', null, noD);
-        No noC = arvore.criarSubArvore('c', noE, noF);
-        arvore.criarRaiz('a', noB, noC);
-        
-        arvore.imprimirPreOrdem();
-    }
+	
+	public static void main(String[] args) {
+			
+		ArvoreBinariaV1 arvore = new ArvoreBinariaV1();
+
+		No noD = arvore.criarNo('d');
+		No noE = arvore.criarNo('e');
+		No noF = arvore.criarNo('f');
+
+		No noB = arvore.criarSubArvore('b', null, noD);
+		No noC = arvore.criarSubArvore('c', noE, noF);
+		arvore.criarRaiz('a', noB, noC);
+
+		System.out.println("Processamento pré-ordem: ");
+		arvore.imprimirPreOrdem();
+
+		System.out.println("\nProcessamento em-ordem: ");
+		arvore.imprimirEmOrdem();
+
+		System.out.println("\nProcessamento pós-ordem: ");
+		arvore.imprimirPosOrdem();
+	}
 }
--- Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
+++ Projeto04/src/br/edu/univas/si3/ed/arvore/model/ArvoreBinariaV1.java
@@ -25,11 +25,36 @@ public class ArvoreBinariaV1 {
 		imprimirPreOrdemInterno(raiz);
 	}
 
-	private void imprimirPreOrdemInterno(No no) {
+	private void imprimirPreOrdemInterno(No no) { //processa antes de ir para a esquerda (recursivo)
 		if (no != null) {
-			System.out.print(no.info + "-");
+			System.out.print(no.info + "-"); //processa a raiz (da sub-árvore)
 			imprimirPreOrdemInterno(no.esq);
 			imprimirPreOrdemInterno(no.dir);
+		}
+	}
+	
+	public void imprimirEmOrdem() {
+		imprimirEmOrdemInterno(raiz);
+	}
+	
+	private void imprimirEmOrdemInterno(No no) { //processa depois que volta da esquerda
+		if (no != null) {
+			imprimirEmOrdemInterno(no.esq);
+			System.out.print(no.info + "-"); //processa a raiz (da sub-árvore)
+			imprimirEmOrdemInterno(no.dir);
+		}
+	}
+	
+	public void imprimirPosOrdem() {
+		imprimirPosrdemInterno(raiz);
+	}
+	
+	private void imprimirPosrdemInterno(No no) { //processa depois que volta da direita
+		if (no != null) {
+			//System.out.println("Processando nó: " + no.info);
+			imprimirPosrdemInterno(no.esq);
+			imprimirPosrdemInterno(no.dir);
+			System.out.print(no.info + "-"); //processa a raiz (da sub-árvore)
 		}
 	}
 }
